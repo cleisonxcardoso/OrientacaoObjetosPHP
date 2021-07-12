1784e17914919d1318ca08462f5e25d25378cd16
Cleison Cardoso <cleison.cardoso@madeiramadeira.com.br>
2021-07-12 02:10:05 -0300

    Aula 4 - Interfaces

--- public/index.php
+++ public/index.php
@@ -2,13 +2,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/../app/model/Caneta.php';
+require_once __DIR__ . '/../app/model/ControleRemoto.php';
 
-$c1 = new Caneta(
-    'BIC',
-    0.5,
-    true,
-    'Azul'
-);
-
-print_r($c1);
+$c = new ControleRemoto();
+$c->maisVolume();
+$c->abrirMenu();
